fe728cbb1168d07ce84aad1709ce7e6c58f973d2
Xavier Gillmann <xgillmann@bluesquarehub.com>
2019-10-09 23:22:57 +0200

    Menu according to rights

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+
+export const RIGHT_REGISTERS=131000
+export const RIGHT_EXTRACTS=131100
+export const RIGHT_REPORTS=131200
+export const RIGHT_UTILITIES=131300
+export const RIGHT_FUNDING=131400
+export const RIGHT_POLICY_RENEWALS=101205 // cfr.UsersBL.vb : that menu entry is provided when user has PolicyRenew!
+export const RIGHT_FEEDBACK_PROMPT=111008 // cfr.UsersBL.vb : that menu entry is provided when user has ClaimReview!
+export const RIGHT_EMAILSETTING=131300
+
