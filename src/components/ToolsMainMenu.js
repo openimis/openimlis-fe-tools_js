70c6e076a35884ab4d0fe3529e0494063f4fa356
Xavier Gillmann <xgillmann@bluesquarehub.com>
2020-11-26 13:33:59 +0100

    inverted withModulesManager injectIntl in menu contrib & bump 1.2.0-rc3

--- src/components/ToolsMainMenu.js
+++ src/components/ToolsMainMenu.js
@@ -83,4 +83,4 @@ const mapStateToProps = state => ({
   rights: !!state.core && !!state.core.user && !!state.core.user.i_user ? state.core.user.i_user.rights : [],
 })
 
-export default withModulesManager(injectIntl(connect(mapStateToProps)(ToolsMainMenu)));
+export default injectIntl(withModulesManager(connect(mapStateToProps)(ToolsMainMenu)));
