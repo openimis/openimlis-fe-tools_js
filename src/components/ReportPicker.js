9aac269ac71baa745bff79fa31702fbd97b6dd7f
lruzicki <56487722+lruzicki@users.noreply.github.com>
2023-02-28 09:39:01 +0100

    OTC-906: added rights check for fetching multiple reports (#23)

--- src/components/ReportPicker.js
+++ src/components/ReportPicker.js
@@ -1,14 +1,16 @@
 import React, { useMemo, useState } from "react";
 import { Autocomplete, useModulesManager, useTranslations } from "@openimis/fe-core";
 import { useReportsQuery } from "../hooks";
+import { RIGHT_REPORTS } from "../constants";
 
 const ReportPicker = (props) => {
   const { onChange, readOnly = false, required = false, withLabel = true, value, label, placeholder } = props;
   const modulesManager = useModulesManager();
   const { formatMessage } = useTranslations("tools", modulesManager);
   const [searchString, setSearchString] = useState("");
-  const { isLoading, data } = useReportsQuery();
-
+  const rights = useSelector((state) => state.core?.user?.i_user?.rights ?? []);
+  const filteredRights = RIGHT_REPORTS.filter(value => rights.includes(value));
+  const { isLoading, data } = useReportsQuery( {rights: filteredRights} );
   const options = useMemo(() => {
     if (searchString) {
       return data?.reports.filter((x) => x.name.toLowerCase().includes(searchString.toLowerCase()));
