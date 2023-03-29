369ffd320318930076d5902f06c2bd49a1c9dddb
lruzicki <56487722+lruzicki@users.noreply.github.com>
2023-03-29 17:05:27 +0200

    OTC-1006: changed rights check for editing reports to be consistent with BE check (#26)
    Co-authored-by: lruzicki <lruzicki@gmail.com>

--- src/components/ReportSearcher.js
+++ src/components/ReportSearcher.js
@@ -1,9 +1,11 @@
 import React, { useCallback, useState } from "react";
+import { useSelector } from "react-redux";
 import { useReportsQuery } from "../hooks";
 import { Searcher, useTranslations, useModulesManager } from "@openimis/fe-core";
 import GenerateReportPicker from "./GenerateReportPicker";
 import { Box, Button } from "@material-ui/core";
 import ReportDefinitionEditorDialog from "./ReportDefinitionEditorDialog";
+import { RIGHT_REPORT_EDIT, RIGHT_REPORT_ADD } from "../constants"
 
 const HEADERS = ["tools.report.description", "tools.report.module", "tools.report.name", ""];
 
@@ -12,6 +14,7 @@ const ReportSearcher = () => {
   const { formatMessageWithValues, formatMessage } = useTranslations("tools", modulesManager);
   const { data, isLoading, error, refetch } = useReportsQuery();
   const [editedReport, setEditedReport] = useState();
+  const rights = useSelector((state) => state.core?.user?.i_user?.rights ?? []);
   const itemFormatters = useCallback(
     () => [
       (r) => r.description,
@@ -19,9 +22,13 @@ const ReportSearcher = () => {
       (r) => r.name,
       (r) => (
         <Box display="flex" justifyContent={"flex-end"} gridGap={12}>
-          <Button onClick={() => setEditedReport(r)} size="small">
-            {formatMessage("ReportSearcher.editBtn")}
-          </Button>
+          {
+            rights.includes(RIGHT_REPORT_ADD) &&
+            rights.includes(RIGHT_REPORT_EDIT) &&
+            <Button onClick={() => setEditedReport(r)} size="small">
+              {formatMessage("ReportSearcher.editBtn")}
+            </Button>
+          }
           <GenerateReportPicker name={r.name} />
         </Box>
       ),
