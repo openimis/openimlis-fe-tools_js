f23b6d19dca4497b2dead60cf9a4a042c5cf672c
wzglinieckisoldevelo <wzgliniecki@soldevelo.com>
2023-04-26 16:28:56 +0200

    OP-1386: Variable name change in extracts.

--- src/pages/ExtractsPage.js
+++ src/pages/ExtractsPage.js
@@ -27,7 +27,7 @@ const OfficerDownloadBlock = (props) => {
     const stringParams = Object.keys(params).map((k)=>`${k}=${encodeURIComponent(params[k])}`)?.join("&")
     return window.open(`${EXTRACTS_URL}/download_${extract}${stringParams ? `?${stringParams}` : ""}`);
   }
-  const officer_code = officer ? decodeId(officer.id) : "";
+  const officer_id = officer ? decodeId(officer.id) : "";
 
   return (
     <Block title={formatMessage("OfficerDownloadBlock.title")}>
@@ -43,13 +43,13 @@ const OfficerDownloadBlock = (props) => {
         </Grid>
         <Grid item xs={6}>
           <Button disabled={!officer} color="primary" variant="contained"
-                  onClick={onExtractDownload("feedbacks", {officer_code})}>
+                  onClick={onExtractDownload("feedbacks", {officer_id})}>
             {formatMessage("OfficerDownloadBlock.downloadFeedbacksBtn")}
           </Button>
         </Grid>
         <Grid item xs={6} align="right">
           <Button disabled={!officer} color="primary" variant="contained"
-                  onClick={onExtractDownload("renewals", {officer_code})}>
+                  onClick={onExtractDownload("renewals", {officer_id})}>
             {formatMessage("OfficerDownloadBlock.downloadRenewalsBtn")}
           </Button>
         </Grid>
