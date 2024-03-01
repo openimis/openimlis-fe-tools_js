00bfbfac6fd46982f8baf63ea009d97d207014bb
olewandowski1 <olewandowski@soldevelo.com>
2024-03-01 16:04:25 +0100

    hotfix: hide tools on moldova instance

--- src/components/ToolsMainMenu.js
+++ src/components/ToolsMainMenu.js
@@ -6,6 +6,11 @@ import { connect } from "react-redux";
 import { RIGHT_REGISTERS, RIGHT_REPORTS, RIGHT_EXTRACTS } from "../constants";
 
 class ToolsMainMenu extends Component {
+  constructor(props) {
+    super(props);
+    this.isWorker = props.modulesManager.getConf("fe-core", "isWorker", false);
+  }
+
   enablers = (enablers) => {
     var e;
     for (e of enablers) {
@@ -15,8 +20,10 @@ class ToolsMainMenu extends Component {
   };
 
   render() {
-    const { rights } = this.props;
+    if (this.isWorker) return null;
+
     let entries = [];
+
     if (this.enablers(RIGHT_REGISTERS)) {
       entries.push({
         text: formatMessage(this.props.intl, "tools", "menu.registers"),
@@ -24,19 +31,6 @@ class ToolsMainMenu extends Component {
         route: "/tools/registers",
       });
     }
-    // Renewals are preformed automatically now.
-    // if (rights.includes(RIGHT_POLICY_RENEWALS)) {
-    //   entries.push(
-    //     { text: formatMessage(this.props.intl, "tools", "menu.renewals"), icon: <Autorenew />, route: "/tools/policyRenewals" }
-    //   )
-    // }
-    // SMS sending feature is normally done automatically now. We'll consider adding this back if necessary
-    // if (rights.includes(RIGHT_FEEDBACK_PROMPT)) {
-    //   entries.push(
-    //     { text: formatMessage(this.props.intl, "tools", "menu.feedbacks"), icon: <Feedback />, route: "/tools/feedbackPrompts" },
-    //   )
-    // }
-    // Extracts page is not implemented in the modular version yet. As it's not used as of now.
     if (this.enablers(RIGHT_EXTRACTS)) {
       entries.push(
         { text: formatMessage(this.props.intl, "tools", "menu.extracts"), icon: <SaveAlt />, route: "/tools/extracts" },
@@ -49,17 +43,9 @@ class ToolsMainMenu extends Component {
         route: "/tools/reports",
       });
     }
-    // if (this.enablers(RIGHT_UTILITIES)) {
-    //   entries.push(
-    //     { text: formatMessage(this.props.intl, "tools", "menu.utilities"), icon: <Build />, route: "/tools/utilities" },
-    //   )
-    // }
-    // if (rights.includes(RIGHT_EMAILSETTING)) {
-    //   entries.push(
-    //     { text: formatMessage(this.props.intl, "tools", "menu.emails"), icon: <EmailOutlined />, route: "/tools/emailSettings" }
-    //   )
-    // }
+
     if (!entries.length) return null;
+    
     return (
       <MainMenuContribution
         {...this.props}
